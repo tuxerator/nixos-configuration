ddb2d9e40b68cb0be5171bb8cce8e06f790774f1
Jakob Sanowski <jakob.sanowski@sanbach.de>
2024-07-31 19:06:51 +0200

    69   2024-07-31 02:01:00   (current)

--- common/greetd.nix
+++ common/greetd.nix
@@ -1,20 +1,10 @@
 { pkgs, lib, config, ... }:
-let
-  sway-config = pkgs.writeText "greetd-sway-config" ''
-    exec "${pkgs.greetd.regreet}/bin/regreet; swaymsg exit"
-    bindsym Mod4+shift+e exec swaynag \
-      -t warning \
-      -m 'What do you want to do?' \
-      -b 'Poweroff' 'systemctl poweroff' \
-      -b 'Reboot' 'systemctl reboot'
-  '';
-in
 {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.gamescope}/bin/gamescope -- ${pkgs.greetd.regreet}/bin/regreet";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time";
       };
     };
   };
