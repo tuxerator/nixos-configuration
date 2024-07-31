cff3618a8cd28f6f97482130e4400094a64f161b
Jakob Sanowski <jakob.sanowski@sanbach.de>
2024-07-31 18:49:03 +0200

    69   2024-07-31 02:01:00   (current)

--- common/greetd.nix
+++ common/greetd.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, ... }:
 let
   sway-config = pkgs.writeText "greetd-sway-config" ''
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+    exec "${pkgs.greetd.regreet}/bin/regreet; swaymsg exit"
     bindsym Mod4+shift+e exec swaynag \
       -t warning \
       -m 'What do you want to do?' \
@@ -14,7 +14,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.sway}/bin/sway --config ${sway-config}";
+        command = "${pkgs.sway}/bin/sway --unsupported-gpu --config ${sway-config}";
       };
     };
   };
