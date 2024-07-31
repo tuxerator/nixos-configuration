e7c3e2da86d733884ece3f98c3166f1f99684ca9
Jakob Sanowski <jakob.sanowski@sanbach.de>
2024-07-31 18:40:56 +0200

    69   2024-07-31 02:01:00   (current)

--- common/greetd.nix
+++ common/greetd.nix
@@ -1,6 +1,5 @@
 { pkgs, lib, config, ... }:
 let
-
   sway-config = pkgs.writeText "greetd-sway-config" ''
     exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
     bindsym Mod4+shift+e exec swaynag \
@@ -15,7 +14,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.dbus}/bin/dbus-run-session${pkgs.dbus}/bin/dbus-run-session  ${pkgs.sway}/bin/sway --config ${sway-config}";
+        command = "${pkgs.sway}/bin/sway --config ${sway-config}";
       };
     };
   };
